6148f341990d57f48b2a8b992d50332119caa0aa
Kenneth Christensen <1kennethac@gmail.com>
2023-12-30 15:26:09 -0700

    Fixed the async rendering problem.
    I had forgotten about making sure to execute the render on the right context. TriggerRender now checks to ensure it is on the context and if not it asks the dispatcher to execute it.

--- Fazor/FazorComponent.cs
+++ Fazor/FazorComponent.cs
@@ -7,13 +7,29 @@ namespace Fazor;
 
 public abstract class FazorComponent : IComponent
 {
+    private readonly RenderFragment _renderFragment;
+    private bool _hasPendingRender = false;
+
+    public FazorComponent()
+    {
+        _renderFragment = builder =>
+        {
+            _hasPendingRender = false;
+            var fragment = GetRenderFragment();
+            fragment(builder);
+        };
+    }
+
+    /// <summary>
+    /// This method exists only so that the Razor source generator has a suitable method to override.
+    /// </summary>
+    /// <param name="builder"></param>
     protected virtual void BuildRenderTree(RenderTreeBuilder builder)
     {
         builder.AddContent(1, "There has been an error!");
     }
-    
+
     private RenderHandle _renderHandle;
-    // private bool _initialized = false;
 
     public void Attach(RenderHandle renderHandle)
     {
@@ -25,24 +41,76 @@ public abstract class FazorComponent : IComponent
 
     public Task SetParametersAsync(ParameterView parameters)
     {
-        // parameters.GetValueOrDefault()
         parameters.SetParameterProperties(this);
         TriggerRender();
         return Task.CompletedTask;
     }
 
-    private void TriggerRender()
+    protected void TriggerRender()
     {
-        _renderHandle.Render(GetRenderFragment());
+        if (_hasPendingRender)
+        {
+            return;
+        }
+
+        _hasPendingRender = true;
+        try
+        {
+            if (!_renderHandle.Dispatcher.CheckAccess())
+            {
+                _renderHandle.Dispatcher.InvokeAsync(() => _renderHandle.Render(_renderFragment));
+            }
+            else
+            {
+                _renderHandle.Render(_renderFragment);
+            }
+        }
+        catch (Exception e)
+        {
+            _hasPendingRender = false;
+            throw;
+        }
     }
 
     protected abstract RenderFragment GetRenderFragment();
 
     #region State Management Methods
 
-    private ConcurrentDictionary<int, object> _states = new();
+    private readonly ConcurrentDictionary<int, object> _states = new();
+    private readonly ConcurrentDictionary<int, (object, object)> _asyncStates = new();
+    private readonly HashSet<Task> _tasksBeingWatched = new();
 
-    public MutableState<TOk> UseState<TOk>(TOk initialValue, [CallerLineNumber] int callerLineNumber = 0)
+    private Task<T> WatchTask<T>(Task<T> task)
+    {
+        if (_tasksBeingWatched.Contains(task) || task.IsCompleted || task.IsFaulted) return task;
+        _ = Task.Run(async () =>
+        {
+            try
+            {
+                await task.ConfigureAwait(false);
+            }
+            finally
+            {
+                _tasksBeingWatched.Remove(task);
+                TriggerRender();
+            }
+        });
+        _tasksBeingWatched.Add(task);
+        return task;
+    }
+
+    private FazorState<TResult, Exception> TaskToState<TResult>(Task<TResult> task)
+    {
+        return WatchTask(task) switch
+        {
+            { IsCompletedSuccessfully: true, Result: var result } => new FazorState<TResult, Exception>.Success(result),
+            { IsCompleted: true, Exception: var exception } => new FazorState<TResult, Exception>.Failure(
+                exception ?? new Exception("Testing....")),
+            _ => FazorState<TResult, Exception>.Loading.Instance
+        };
+    }
+
+    protected MutableState<TOk> UseState<TOk>(TOk initialValue, [CallerLineNumber] int callerLineNumber = 0)
     {
         var value = _states.GetOrAdd(callerLineNumber, _ => initialValue!);
         return new MutableState<TOk>()
@@ -54,6 +122,23 @@ public abstract class FazorComponent : IComponent
                 TriggerRender();
             }
         };
+    }
+
+    protected FazorState<TOk, Exception> Derive<TInput, TOk>(Func<TInput, Task<TOk>> derivation, TInput input,
+        [CallerLineNumber] int callerLineNumber = 0)
+    {
+        var thisInput = new FazorState<TInput, Exception>.Success(input);
+        var (lastInputObject, derivationTask)
+            = ((FazorState<TInput, Exception>, Task<TOk>))_asyncStates.GetOrAdd(
+                callerLineNumber,
+                _ => (thisInput, derivation(input)));
+        if (lastInputObject!.Equals(thisInput)) return TaskToState(derivationTask);
+
+        var newDerivation = derivation(input);
+        _asyncStates.AddOrUpdate(callerLineNumber,
+            _ => (thisInput, newDerivation),
+            (_, _) => (thisInput, newDerivation));
+        return TaskToState(newDerivation);
     }
 
     #endregion
@@ -79,10 +164,14 @@ public abstract record FazorState<TOk, TErr>
         {
         }
 
-        public static Loading Instance = new();
+        public static readonly Loading Instance = new();
     }
 
     public record Success(TOk Value) : FazorState<TOk, TErr>;
 
     public record Failure(TErr Value) : FazorState<TOk, TErr>;
+
+    public Success? AsSuccess() => this as Success;
+    public Failure? AsFailure() => this as Failure;
+    public Loading? AsLoading() => this as Loading;
 }
